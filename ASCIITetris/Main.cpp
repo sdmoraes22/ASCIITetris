99816d69461356219f61fc6320aea8cbb4b381c0
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-09-29 22:23:25 -0300

    Collision detection

--- ASCIITetris/Main.cpp
+++ ASCIITetris/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 
-std::wstring tetrimino[7];
+std::wstring tetromino[7];
 
 int nFieldWidth = 12;
 int nFieldHeight = 18;
@@ -23,42 +23,66 @@ int rotate(int px, int py, int r)
 	return 0;
 }
 
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	for(int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = rotate(px, py, nRotation);
+
+			// Get index into the field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py > 0 && nPosY + py < nFieldHeight) 
+				{
+					if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+						return false;
+				}
+			}
+		}
+
+	return true;
+}
+
 int main()
 {
-	tetrimino[0].append(L"..X.");
-	tetrimino[0].append(L"..X.");
-	tetrimino[0].append(L"..X.");
-	tetrimino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
 
-	tetrimino[1].append(L"..X.");
-	tetrimino[1].append(L".XX.");
-	tetrimino[1].append(L".X..");
-	tetrimino[1].append(L"....");
+	tetromino[1].append(L"..X.");
+	tetromino[1].append(L".XX.");
+	tetromino[1].append(L".X..");
+	tetromino[1].append(L"....");
 
-	tetrimino[2].append(L".X..");
-	tetrimino[2].append(L".XX.");
-	tetrimino[2].append(L"..X.");
-	tetrimino[2].append(L"....");
+	tetromino[2].append(L".X..");
+	tetromino[2].append(L".XX.");
+	tetromino[2].append(L"..X.");
+	tetromino[2].append(L"....");
 
-	tetrimino[3].append(L"....");
-	tetrimino[3].append(L".XX.");
-	tetrimino[3].append(L".XX.");
-	tetrimino[3].append(L"....");
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L"....");
 
-	tetrimino[4].append(L"..X.");
-	tetrimino[4].append(L".XX.");
-	tetrimino[4].append(L"..X.");
-	tetrimino[4].append(L"....");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L".XX.");
+	tetromino[4].append(L"..X.");
+	tetromino[4].append(L"....");
 	
-	tetrimino[5].append(L"....");
-	tetrimino[5].append(L".XX.");
-	tetrimino[5].append(L"..X.");
-	tetrimino[5].append(L"..X.");
+	tetromino[5].append(L"....");
+	tetromino[5].append(L".XX.");
+	tetromino[5].append(L"..X.");
+	tetromino[5].append(L"..X.");
 
-	tetrimino[6].append(L"....");
-	tetrimino[6].append(L".XX.");
-	tetrimino[6].append(L".X..");
-	tetrimino[6].append(L".X..");
+	tetromino[6].append(L"....");
+	tetromino[6].append(L".XX.");
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".X..");
 
 	pField = new unsigned char[nFieldWidth * nFieldHeight];
 	
